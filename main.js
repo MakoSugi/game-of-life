4c053b1bcf968dbbe0b12fb5e0c601ab6fe201cf
MakoSugi <m7125m@gmail.com>
2026-03-02 20:57:39 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 import randomArray2D from './random-array-2d.js';
 import changeArray2D from './change-array-2d.js';
-console.log('Phaser Version:', Phaser.VERSION);
 class Example extends Phaser.Scene {
   constructor() {
     super('Example');
@@ -9,7 +8,7 @@ class Example extends Phaser.Scene {
     this.array2D = randomArray2D(this.ARRAY_SIZE);
   }
   update() {
-    console.log("FPS:", this.game.loop.actualFps);
+    console.log("FPS:", game.loop.actualFps);
     this.drawRectangle();
     this.array2D = changeArray2D(this.array2D);
     this.scene.restart();
