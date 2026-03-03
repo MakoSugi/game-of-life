e1158d43b217beea699a9b7089dcfb7bb905632d
MakoSugi <m7125m@gmail.com>
2026-03-03 20:08:51 +0900

    Add files via upload

---
+++ game-of-life.js
@@ -0,0 +1,26 @@
+import randomArray2D from './random-array-2d.js';
+import changeArray2D from './change-array-2d.js';
+export default class GameOfLife extends Phaser.Scene {
+  constructor() {
+    super('Example');
+    this.ARRAY_SIZE = 100;
+    this.RECTANGLE_SIZE = 5;
+    this.array2D = randomArray2D(this.ARRAY_SIZE);
+  }
+  update() {
+    console.log("FPS:", this.game.loop.actualFps);
+    this.drawRectangle();
+    this.array2D = changeArray2D(this.array2D);
+    this.scene.restart();
+  }
+  drawRectangle() {
+    for (let i = 0; i < this.ARRAY_SIZE; i++) {
+      for (let j = 0; j < this.ARRAY_SIZE; j++) {
+        if (this.array2D[i][j] == 1) {
+          this.add.rectangle(this.RECTANGLE_SIZE * i, this.RECTANGLE_SIZE * j,
+            this.RECTANGLE_SIZE - 1, this.RECTANGLE_SIZE - 1, 0xffffff);
+        }
+      }
+    }
+  }
+}
